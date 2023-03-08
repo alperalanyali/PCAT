267f858d6ea7ff76f02cf8fb87b3a87e7b823af0
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-03-08 19:10:49 +0300

    .

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.get('/',(req,res)=>{  
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
 
 const port = 3000;
 
